a327a111ec7e07e4ebd0999326c54e44536b3867
ncrohn <nick@meltmedia.com>
2012-03-21 18:30:32 -0700

    Started file upload bit

--- public/js/test-post.js
+++ public/js/test-post.js
@@ -2,6 +2,63 @@ $(document).ready(
   function() {
 
     var $test = $("#test"),
-        entryUri = '/add-entry';
+    entryUri = '/add-entry',
+    token = 'ZzP5jJSWknXiZ88Wl5gXJcRdK7KBWE';
+
+    var callback = function(event) {
+      event.preventDefault();
+
+              console.log('hi');
+
+        var serializedData = $test.serializeArray(),
+            i=0, data = {};
+
+        for(i;i<serializedData.length;i++) {
+          var d = serializedData[i];
+
+          data[d.name] = d.value;
+        }
+
+        data.token = token;
+
+        $.ajax({
+          url: entryUri,
+          data: JSON.stringify(data),
+          type: 'POST',
+          dataType: 'json',
+          processData: false,
+          contentType: 'application/json',
+          success: function(d) {
+            entryUri = entryUri + '/' + d.entryId;
+
+            $test.action = entryUri;
+
+            $test.unbind('submit', callback);
+            $test.submit(
+              function(event) {
+                event.preventDefault();
+                var file = $('input[name="image"]')[0].files[0];
+                $.ajax({
+                  url: entryUri,
+                  data: file,
+                  type: 'POST',
+                  processData: false,
+                  contentType: 'application/octet-stream',
+                  headers: {'X-File-Name': file.fileName},
+                  progress: function() {
+                    console.log('progress');
+                  },
+                  success: function() {
+                    console.log('done');
+                  }
+
+                });
+              });
+            $test.submit();
+          }
+        });
+    };
+
+    $test.submit(callback);
 
   });
